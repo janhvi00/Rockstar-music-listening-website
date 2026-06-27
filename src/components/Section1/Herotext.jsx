921667176b1bcb0267fb15ea4ba9c028609e1608
Janhvi <janhvi571@gmail.com>
2026-06-27 06:17:21 +0530

    he he

--- src/components/Section1/Herotext.jsx
+++ src/components/Section1/Herotext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Herotext = () => {
   return (
-    <div className = "p-6">
+    <div className = "p-3">
         <h1 className = "mb-7 text-5xl pt-6 font-bold leading-[1.3]">Rockstar: Pain <br/><span className = "bg-red-200 rounded-xl">creates</span> <br/> Art - Jordan</h1>
         <p className = "text-2xl w-[90%] italic">Pata hai, <br/>Yaha se bahut dur sahi or galat ke par, <br/>Ek maidan hai main vahi milunga tujhe <br/>- Jordan</p>
     </div>
